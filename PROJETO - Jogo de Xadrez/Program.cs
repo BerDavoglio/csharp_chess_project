83520c970ee25cf494d32eadd5cbabdc41795e4e
BerDavoglio <berdavoglio@gmail.com>
2020-05-30 13:09:01 -0300

    Iniciando projeto de xadrez.

---
+++ PROJETO - Jogo de Xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROJETO___Jogo_de_Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
