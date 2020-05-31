341bf8a8b896457f2ff13d30b3d9fd01e386ed80
BerDavoglio <berdavoglio@gmail.com>
2020-05-31 00:35:50 -0300

    Adicionando uma forma melhor de movimentação pelo tabuleiro.

--- PROJETO - Jogo de Xadrez/Program.cs
+++ PROJETO - Jogo de Xadrez/Program.cs
@@ -11,13 +11,11 @@ namespace PROJETO___Jogo_de_Xadrez
         {
             try
             {
-                Board board = new Board(8, 8);
+                ChessPosition pos = new ChessPosition('a', 1);
 
-                board.InsertPiece(new Rook(Color.Black, board), new Position(0, 0));
-                board.InsertPiece(new Rook(Color.Black, board), new Position(1, 3));
-                board.InsertPiece(new King(Color.Black, board), new Position(2, 4));
+                Console.WriteLine(pos);
 
-                Screen.PrintScream(board);
+                Console.WriteLine(pos.ToPosition());
             }
             catch (BoardException e)
             {
